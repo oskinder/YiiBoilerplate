bafca4d86dc05ec5e59268db9fd07175f41d7113
Oleg Skinder <oleg.skinder@gmail.com>
2014-04-21 12:54:22 +0300

    Fix syntax violations

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -8,10 +8,14 @@
  */
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -21,7 +25,7 @@
 
 <body>
 <!-- NAVIGATION BEGIN -->
-<?php $this->renderPartial('//layouts/_navigation');?>
+<?php $this->renderPartial('//layouts/_navigation'); ?>
 <!-- NAVIGATION END -->
 
 <!-- CONTENT WRAPPER BEGIN -->
@@ -33,24 +37,24 @@
                 'links' => $this->breadcrumbs,
             )
         ); ?>
-    <?php endif?>
+    <?php endif ?>
 
-	<div class="row">
+    <div class="row">
 
         <!-- CONTENT BEGIN -->
-		<?= $content; ?>
+        <?= $content; ?>
         <!-- CONTENT END -->
 
     </div>
 
     <div class="row">
         <hr/>
-		<footer>
-			Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-			All Rights Reserved.<br/>
-			<?= Yii::powered(); ?>
-		</footer>
-	</div>
+        <footer>
+            Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+            All Rights Reserved.<br/>
+            <?= Yii::powered(); ?>
+        </footer>
+    </div>
 
 </div>
 <!-- CONTENT WRAPPER END -->
--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -13,40 +13,40 @@ $this->breadcrumbs = ['Login'];
 <!-- Login Form BEGIN -->
 <div class="form">
 
-<?php
-/** @var TbActiveForm $form */
-$form = $this->beginWidget(
-	'bootstrap.widgets.TbActiveForm',
-	array(
-		'id' => 'login-form',
-		'enableClientValidation' => true,
-		'htmlOptions' => ['class' => 'well'],
-		'clientOptions' => array(
-			'validateOnSubmit'=>true,
-		),
-	)
-);
+    <?php
+    /** @var TbActiveForm $form */
+    $form = $this->beginWidget(
+        'bootstrap.widgets.TbActiveForm',
+        array(
+            'id' => 'login-form',
+            'enableClientValidation' => true,
+            'htmlOptions' => ['class' => 'well'],
+            'clientOptions' => array(
+                'validateOnSubmit' => true,
+            ),
+        )
+    );
 
-echo CHtml::errorSummary($model, null, null, array('class' => 'alert alert-error'));
-?>
+    echo CHtml::errorSummary($model, null, null, array('class' => 'alert alert-error'));
+    ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?= $form->textFieldRow($model, 'username', array('class'=>'span3'));?>
-	<?= $form->passwordFieldRow($model, 'password', array('class'=>'span3'));?>
-	<?= $form->checkBoxRow($model, 'rememberMe');?>
+    <?= $form->textFieldRow($model, 'username', array('class' => 'span3')); ?>
+    <?= $form->passwordFieldRow($model, 'password', array('class' => 'span3')); ?>
+    <?= $form->checkBoxRow($model, 'rememberMe'); ?>
 
-	<?php if ($model->isCaptchaRequired()): ?>
-		<?php $this->widget('CCaptcha'); ?>
-		<?= $form->textField($model, 'verifyCode'); ?>
-	<?php endif; ?>
+    <?php if ($model->isCaptchaRequired()): ?>
+        <?php $this->widget('CCaptcha'); ?>
+        <?= $form->textField($model, 'verifyCode'); ?>
+    <?php endif; ?>
 
-	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit','type'=>'primary','label'=>'Submit', 'icon'=>'ok'));?>
-		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'reset','label'=>'Reset'));?>
-	</div>
+    <div class="form-actions">
+        <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType' => 'submit', 'type' => 'primary', 'label' => 'Submit', 'icon' => 'ok')); ?>
+        <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType' => 'reset', 'label' => 'Reset')); ?>
+    </div>
 
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div>
 <!-- Login Form END -->
